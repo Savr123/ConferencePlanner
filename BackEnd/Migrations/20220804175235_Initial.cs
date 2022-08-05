02b4a4c5f105b49a1efb0b03e2c816a22afed728
Savr123 <Sangievv@mail.ru>
2022-08-05 15:56:23 +0300

    init: passed 1-st session of workshop plan and created aspdotnet app with sqlite db

---
+++ BackEnd/Migrations/20220804175235_Initial.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BackEnd.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Speakers",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 200, nullable: false),
+                    Bio = table.Column<string>(type: "TEXT", maxLength: 250, nullable: true),
+                    WebSite = table.Column<string>(type: "TEXT", maxLength: 1000, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Speakers", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Speakers");
+        }
+    }
+}
---
+++ BackEnd/Models/Speaker.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BackEnd.Models
+{
+    public class Speaker
+    {
+        public int id { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string? Name { get; set; }
+
+        [StringLength(250)]
+        public string? Bio { get; set; }
+
+        [StringLength(1000)]
+        public virtual string? WebSite { get; set; }
+
+    }
+}
